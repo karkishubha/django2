3ca16d859ee992f73caf8f36e81fef369bafcccb
Shubha Raj Karki <karkishubha985@gmail.com>
2025-07-18 11:04:10 +0545

    added context

--- testapp/urls.py
+++ testapp/urls.py
@@ -5,5 +5,8 @@ from testapp import views as testapp_views
 urlpatterns = [
     
     path('home/', testapp_views.home,name="testhome"),
+    path('about/', testapp_views.about,name="testabout"),
+    
+
 
 ]
--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def home(request):
-    return render(request, 'test/home.html')
+    current_dt= datetime.datetime.now()
+    return render(request, 'test/home.html',{'dt':current_dt})
+def about(request):
+    info={'name':"Shubha",'address':"balkot",'number':986623632}
+    return render(request, 'test/about.html',{'my_info':info})
